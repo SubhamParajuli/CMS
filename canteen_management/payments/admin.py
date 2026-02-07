b31b38ad95de7149a9f267c22ec87b30afd6abfc
Subham Parajuli <suvamparajuli222@gmail.com>
2026-02-07 18:16:21 +0545

    payment model completed

--- canteen_management/payments/admin.py
+++ canteen_management/payments/admin.py
@@ -1,3 +1,15 @@
-from django.contrib import admin
+
 
 # Register your models here.
+from django.contrib import admin
+from .models import Payment, Receipt
+
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ('order', 'amount_paid', 'payment_method', 'payment_time')
+
+
+@admin.register(Receipt)
+class ReceiptAdmin(admin.ModelAdmin):
+    list_display = ('order', 'generated_at')
--- canteen_management/payments/models.py
+++ canteen_management/payments/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from orders.models import *
 
 # Create your models here.
+
+class Payment(models.Model):
+    order=models.OneToOneField(
+        Order,
+        on_delete=models.CASCADE
+    )
+
+    payment_method=models.CharField(default='CASH')
+
+    amount_paid=models.DecimalField(
+        max_digits=8,
+        decimal_places=2
+    )
+
+    payment_time= models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Payment for Order #{self.order.id}"
+    
+
+
+class Receipt(models.Model):
+    order = models.OneToOneField(
+        Order,
+        on_delete=models.CASCADE
+    )
+    generated_at = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return f"Receipt for Order #{self.order.id}"
+    
+
